ddc860d57836808aa5393d82044283b6b5775933
José Ventura <joseventura@Joses-MacBook-Pro-2.local>
2022-03-13 00:26:32 +0100

    FEAT iteration 1,2,3,4,5

---
+++ js-basic-algorithms/iteration1.js
@@ -0,0 +1,7 @@
+let myFovoriteHero = "Hulk";
+let x = 50;
+let h = 5
+let y = 10;
+let z = h + y;
+
+console.log(z);
---
+++ js-basic-algorithms/iteration3.js
@@ -0,0 +1,34 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+// let numA = 10;
+// let numB = 5;
+// let result = (numA * numB);
+// // alert(result);
+// console.log(result);
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+// let numA = 10;
+// let numB = 2;
+// let result = (numA / numB);
+// // alert(result);
+// console.log(result);
+
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+// let numA = 15;
+// let numB = 9;
+// let result = (numA % numB);
+// // alert(result);
+// console.log(result);
+
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// teniendo dos variables y = 10 y z = 5.
+// let y = 10;
+// let z = 5;
+// let x = (y + z);
+// console.log(x);
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+// let y = 10;
+// let z = 5;
+// let x = (y * z);
+// console.log(x);
